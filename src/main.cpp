10b8e51b090a795fe8f71c4fbf16d2614f4bb83f
tute-avalos <msavalos@gmail.com>
2024-11-13 16:49:03 -0300

    Se añade .clang-format para mejorar el formateo del código

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,23 @@
  * @brief LED API RESTful
  * @version 0.1
  * @date 2024-11-08
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  * Copyright (C) 2024 Matías S. Ávalos (tute-avalos.com)
  *
  * This file is part of LED API RESTful.
- * 
+ *
  * LED API RESTful is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * LED API RESTful is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with LED API RESTful.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -49,10 +49,8 @@ const char *LIMIT_REACHED = "{\"error\":\"Límite de LEDs alcanzado.\"}\n";
 const char *INVALID_JSON = "{\"error\":\"JSON inválido.\"}\n";
 const char *INVALID_STATE = "{\"error\":\"'estado' inválido.\"}\n";
 const char *NOT_FOUND = "{\"error\":\"'id' no encontrado.\"}\n";
-const char *MISSING_PIN_LOGIC =
-    "{\"error\":\"Faltan campos obligatorios: 'pin' y 'logica'.\"}\n";
-const char *MISSING_LED_STATE =
-    "{\"error\":\"Faltan el campo obligatorio: 'estado'.\"}\n";
+const char *MISSING_PIN_LOGIC = "{\"error\":\"Faltan campos obligatorios: 'pin' y 'logica'.\"}\n";
+const char *MISSING_LED_STATE = "{\"error\":\"Faltan el campo obligatorio: 'estado'.\"}\n";
 
 // Cantidad máxima de LEDs a crear/registrar.
 const size_t MAX_LEDS{5};
@@ -78,14 +76,14 @@ void handleRequestBody(uint8_t *data, size_t len, String &body) {
 
 /**
  * @brief Endpoints GET /led y /led/{n}
- * 
+ *
  * En caso de solicitar /led se devuelve una lista en formato JSON de los LEDs
  * creados hasta el momento.
- * 
+ *
  * En caso de solicitar /led/{n}, n indica el id del LED solicitado y devuelve
  * el JSON del estado acutal del LED (200) o un mensaje que no pudo encontrarse
  * dicho LED (404).
- * 
+ *
  * @param req AsyncWebServerRequest* solicitud recibida por el cliente
  */
 void readLed(AsyncWebServerRequest *req) {
@@ -103,8 +101,10 @@ void readLed(AsyncWebServerRequest *req) {
         break;
       }
     }
-    if (led_found) req->send(200, "application/json", led_found->toJson() + '\n');
-    else req->send(404, "application/json", NOT_FOUND);
+    if (led_found)
+      req->send(200, "application/json", led_found->toJson() + '\n');
+    else
+      req->send(404, "application/json", NOT_FOUND);
 
   } else {
     // *** Si se pidieron todos los LEDs ***
@@ -122,21 +122,20 @@ void readLed(AsyncWebServerRequest *req) {
 
 /**
  * @brief Endpoint POST /led
- * 
+ *
  * Se recibe un json con el formato {"pin": n, "logica": l} donde 'n' es el número del
  * pin donde se encuentra conectado el LED y 'l' es la lógica que utiliza para encender:
- * 
+ *
  *    - "HIGH": El LED enciende con HIGH (ánodo conectado al pin y cátodo a GND)
  *    - "LOW": El LED enciende con LOW (cátodo conectado al pin y ánodo a +V)
- * 
+ *
  * @param req AsyncWebServerRequest* request del cliente
  * @param data uint8_t* información del body del paquete actual
  * @param len size_t longitud del paquete actual
  * @param index size_t indice del paquete actual
  * @param total size_t tamaño total del body
  */
-void createLed(AsyncWebServerRequest *req, uint8_t *data, size_t len,
-               size_t index, size_t total) {
+void createLed(AsyncWebServerRequest *req, uint8_t *data, size_t len, size_t index, size_t total) {
   static String body{};
   if (!index) body = "";
 
@@ -174,20 +173,19 @@ void createLed(AsyncWebServerRequest *req, uint8_t *data, size_t len,
 
 /**
  * @brief Endpoint PATCH /led/{n}
- * 
+ *
  * Se recibe un json con el formato {"estado": e } donde 'e' puede ser:
  *    - "on": prende el led
  *    - "off": apaga el led
  *    - "toggle": alterna el led
- * 
+ *
  * @param req AsyncWebServerRequest* request del cliente
  * @param data uint8_t* información del body del paquete actual
  * @param len size_t longitud del paquete actual
  * @param index size_t indice del paquete actual
  * @param total size_t tamaño total del body
  */
-void updateLed(AsyncWebServerRequest *req, uint8_t *data, size_t len,
-               size_t index, size_t total) {
+void updateLed(AsyncWebServerRequest *req, uint8_t *data, size_t len, size_t index, size_t total) {
   static String body{};
   if (!index) body = "";
 
@@ -210,28 +208,33 @@ void updateLed(AsyncWebServerRequest *req, uint8_t *data, size_t len,
     for (auto &led : leds) {
       if (led != nullptr && led->getID() == id) {
         led_found = led;
-        if (estado == "on") led->on();
-        else if (estado == "off") led->off();
-        else if (estado == "toggle") led->toggle();
-        else {
+        if (estado == "on") {
+          led->on();
+        } else if (estado == "off") {
+          led->off();
+        } else if (estado == "toggle") {
+          led->toggle();
+        } else {
           req->send(409, "application/json", INVALID_STATE);
           return;
         }
         break;
       }
     }
-    if (led_found) req->send(200, "application/json", led_found->toJson() + '\n');
-    else req->send(404, "application/json", NOT_FOUND);
+    if (led_found)
+      req->send(200, "application/json", led_found->toJson() + '\n');
+    else
+      req->send(404, "application/json", NOT_FOUND);
   }
 }
 
 /**
  * @brief Endpint DELETE /led/{n}
- * 
+ *
  * Se procede a buscar el led con id n, si se encuentra se envía el mensaje con el
  * mensaje de éxito (200), de lo contrario se envía un mensaje de que el led no se
  * encontró (404).
- * 
+ *
  * @param req AsyncWebServerRequest* request del cliente.
  */
 void deleteLed(AsyncWebServerRequest *req) {
@@ -248,12 +251,13 @@ void deleteLed(AsyncWebServerRequest *req) {
   }
   if (isDeleted)
     req->send(200, "application/json", "{\"message\":\"LED '" + req->pathArg(0) + "' borrado.\"}\n");
-  else req->send(404, "application/json", NOT_FOUND);
+  else
+    req->send(404, "application/json", NOT_FOUND);
 }
 
 /**
  * @brief Se inicializa el arreglo de Leds, inicialmente todos a nullptr
- * 
+ *
  */
 void initLEDArray() {
   for (auto &led : leds) led = nullptr;
@@ -261,9 +265,9 @@ void initLEDArray() {
 
 /**
  * @brief Configuraciones iniciales
- * 
+ *
  * Aquí se inicializa el puerto Serial, WiFi y cualquier otro periférico que sea necesario.
- * 
+ *
  */
 void initConfig() {
   Serial.begin(SBAUDRATE);
@@ -282,16 +286,18 @@ void initConfig() {
 
 /**
  * @brief Configuración del servidor
- * 
+ *
  * Se configuran los endpoints:
  * GET    /led      : obtiene todos los leds creados
  * GET    /led/{n}  : obtiene el led con id "n"
  * POST   /led      : crea un led (formato: {"pin": n, "logica": ("HIGH"|"LOW")})
- * PATCH  /led/{n}  : Modifica el estado del led con id "n" (formato: {"estado":("on"|"off"|"toggle")})
+ * PATCH  /led/{n}  : Modifica el estado del led con id "n" (formato:
+ *                                              {"estado":("on"|"off"|"toggle")})
+ *
  * DELETE /led/{n}  : Borra el led con id "n"
- * 
+ *
  * Se configura el CORS para aceptar peticiones desde los navegadores.
- * 
+ *
  * Y se inicializa el servidor para recibir las peticiones.
  */
 void initServer() {
@@ -301,11 +307,9 @@ void initServer() {
   });
   server.on("^\\/led(\\/[0-9]+)?$", HTTP_GET, readLed);
   server.on("/led", HTTP_POST, [](AsyncWebServerRequest *req) {}, NULL, createLed);
-  server.on(
-      "^\\/led\\/([0-9]+)$", HTTP_PATCH, [](AsyncWebServerRequest *req) {},
-      NULL, updateLed);
+  server.on("^\\/led\\/([0-9]+)$", HTTP_PATCH, [](AsyncWebServerRequest *req) {}, NULL, updateLed);
   server.on("^\\/led\\/([0-9]+)$", HTTP_DELETE, deleteLed);
-  
+
   /**********************  Configuración para el CORS **********************/
   server.on("/led", HTTP_OPTIONS, [](AsyncWebServerRequest *req) {
     AsyncWebServerResponse *res = req->beginResponse(204);
